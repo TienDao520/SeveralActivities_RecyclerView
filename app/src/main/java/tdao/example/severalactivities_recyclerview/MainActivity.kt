c713bd23802d6fa2cfaf4141998ee3b47e7523b6
Tien Dao <tiendao5240@gmail.com>
2022-02-11 11:59:57 -0500

    Task1 Companion Object: Launch userName to textViewName

--- app/src/main/java/tdao/example/severalactivities_recyclerview/MainActivity.kt
+++ app/src/main/java/tdao/example/severalactivities_recyclerview/MainActivity.kt
@@ -1,18 +1,22 @@
 package tdao.example.severalactivities_recyclerview
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
 
     // companion object
     companion object MainActivityData {
         /**Task1-2: Create userName inside companion object */
         var userName:String = ""
     }
+
+    lateinit var textViewName: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,4 +29,11 @@ class MainActivity : AppCompatActivity() {
         val intent: Intent = Intent(this,NameActivity::class.java)
         startActivity(intent)
     }
+
+    override fun onResume() {
+        super.onResume()
+        /**Task1-4: Launch userName to textViewName */
+        textViewName.text = userName
+    }
+
 }
